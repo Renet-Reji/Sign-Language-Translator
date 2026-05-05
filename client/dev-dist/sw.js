debb71f8f7afb1deced550d8d2b8a6c6c1c22835
rishikesh-babu <rishikeshbabu2004@gmail.com>
2026-05-05 17:44:29 +0530

    Add favicon

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.flm0i607qko"
+    "revision": "0.hf92a453t04"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
